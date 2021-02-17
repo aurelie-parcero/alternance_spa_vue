2780a55d58d0ae5945e0a353bd0ae64ae4a2f441
aurelie-parcero <aurelie.parcero@le-campus-numerique.fr>
2021-02-17 14:06:54 +0100

    plus d appels a mapbox gl

--- src/components/CitiesMap.vue
+++ src/components/CitiesMap.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <h1>Carte des villes</h1>
-    <mapbox
-        :map-options="{
-        style: 'https://maps.hotentic.com/styles/isere/style.json',
-        center: [6.0925617, 45.8910906],
-        zoom: 10,
-      }"
-        @map-load="loaded"
-    />
+<!--    <mapbox-->
+<!--        :map-options="{-->
+<!--        style: 'https://maps.hotentic.com/styles/isere/style.json',-->
+<!--        center: [6.0925617, 45.8910906],-->
+<!--        zoom: 10,-->
+<!--      }"-->
+<!--        @map-load="loaded"-->
+<!--    />-->
   </div>
 </template>
 
 <script>
   import axios from "axios";
-  import Mapbox from 'mapbox-gl-vue';
+  //import Mapbox from 'mapbox-gl-vue';
 
   export default {
     name: 'CitiesMap',
     components: {
-      Mapbox
+      //Mapbox
     },
     data() {
       return {
@@ -33,20 +33,20 @@
           this.cities.push({name, lat, lon, weather, icon, temperature, updatedAt: new Date(updatedAt * 1000)});
         }
       },
-      loaded(map) {
-        const mapboxgl = require('mapbox-gl/dist/mapbox-gl');
+      // loaded(map) {
+      //   //const mapboxgl = require('mapbox-gl/dist/mapbox-gl');
+      //
+      //   this.cities.forEach(function(city) {
+      //     let el = document.createElement('img');
+      //     el.src = `https://openweathermap.org/img/wn/${city.icon}@2x.png`;
+      //     el.classList.add('marker');
+      //     el.title = `${city.name} - ${city.temperature}°C`;
 
-        this.cities.forEach(function(city) {
-          let el = document.createElement('img');
-          el.src = `https://openweathermap.org/img/wn/${city.icon}@2x.png`;
-          el.classList.add('marker');
-          el.title = `${city.name} - ${city.temperature}°C`;
-
-          new mapboxgl.Marker(el)
-            .setLngLat([city.lon, city.lat])
-            .addTo(map);
-        });
-      },
+          // new mapboxgl.Marker(el)
+          //   .setLngLat([city.lon, city.lat])
+          //   .addTo(map);
+        // });
+      // },
     },
     mounted() {
       axios.get(`https://api.openweathermap.org/data/2.5/find?lat=${process.env.VUE_APP_DEFAULT_LATITUDE}&lon=${process.env.VUE_APP_DEFAULT_LONGITUDE}&cnt=20&cluster=yes&lang=fr&units=metric&APPID=${process.env.VUE_APP_OW_APP_ID}`)
